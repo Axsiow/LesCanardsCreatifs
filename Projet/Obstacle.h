a2b76aae65b9ff931ccb46ab1be7ed4f457276c0
Casser <paco.hanafi@epsi.fr>
2019-04-16 16:22:27 +0200

    Copier coller tout
    On remet le travail

--- Projet/Obstacle.h
+++ Projet/Obstacle.h
@@ -1,8 +1,23 @@
 #pragma once
-class Obstacle
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Entite.h"
+using namespace std;
+
+class Obstacle : public Entite
 {
 public:
 	Obstacle();
+	Obstacle(string symbole);
 	~Obstacle();
+
+	// get set vertical
+	bool getVertcal();
+	void setVertical(bool vertical);
+
+private:
+	bool m_vertical;
+
 };
 
