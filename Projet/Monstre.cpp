9e619c5ff229bf62938883502981037c2bd5b90a
Théo <theo_38180@live.fr>
2019-04-16 16:37:10 +0200

    Revert "Revert "Copier coller tout""
    This reverts commit 46caf6192a9f731c5b024d94824efc5701fc8a18.

--- Projet/Monstre.cpp
+++ Projet/Monstre.cpp
@@ -6,7 +6,20 @@ Monstre::Monstre()
 {
 }
 
+Monstre::Monstre(string symbole)
+{
+	m_affichage = symbole;
+}
+
 
 Monstre::~Monstre()
+{
+}
+
+void Monstre::deplacement()
+{
+}
+
+void Monstre::monstre(string)
 {
 }
--- Projet/Projet.cpp
+++ Projet/Projet.cpp
@@ -3,5 +3,5 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Grosse truie\n"; 
 }
