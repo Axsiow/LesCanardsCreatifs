4fedb4384cfef381f84a9f68d93b57401d5dffc6
Théo <theo_38180@live.fr>
2019-04-16 14:49:19 +0200

    Modification de la classe Hero
    Ajout du squelette des méthodes

--- Projet/Heros.h
+++ Projet/Heros.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
 class Heros
 {
 public:
 	Heros();
+	Heros(string nom, int nb_vie, int jsp);
 	~Heros();
+
+	void deplacer();
+	int getVie();
+	void setVie(int nb_vie);
+	void activerPouvoir();
+
+private:
+	int nb_vie = 3;
 };
 
