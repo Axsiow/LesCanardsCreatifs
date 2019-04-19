b8f79aeff79e4d98b93a4673174e10ec87c0fe8d
Casser <paco.hanafi@epsi.fr>
2019-04-19 15:19:08 +0200

    Monstre fonction
    ajout des fonctions sur monstre et le constructeur avec les fonctions random

--- Projet/Monstre.cpp
+++ Projet/Monstre.cpp
@@ -9,6 +9,10 @@ Monstre::Monstre()
 Monstre::Monstre(string symbole)
 {
 	m_affichage = symbole;
+	srand(time(NULL));
+	m_positionX= rand() % (11);
+	m_positionY= rand() % (11);
+
 }
 
 
@@ -18,8 +22,22 @@ Monstre::~Monstre()
 
 void Monstre::deplacement()
 {
-}
-
-void Monstre::monstre(string)
-{
+	srand(time(NULL));
+	int direction = rand() % (4);
+	if (direction == 0)
+	{
+		m_positionY = m_positionY + 1;
+	}
+	else if(direction == 1)
+	{
+		m_positionX = m_positionX + 1;
+	}
+	else if (direction == 2)
+	{
+		m_positionY = m_positionY - 1;
+	}
+	else
+	{
+		m_positionX = m_positionX - 1;
+	}
 }
--- Projet/Projet.cpp
+++ Projet/Projet.cpp
@@ -1,7 +1,35 @@
 #include "pch.h"
 #include <iostream>
+#include <random>
+#include <time.h>
+#include "Entite.h"
+#include "Heros.h" 
+#include"Monstre.h"
+#include"Obstacle.h"
+#include"Partie.h"
+#include"Plateau.h"
+#include"Pouvoir.h"
+#include"Score.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Grosse truie\n"; 
+    cout << "Coucou\n"; 
+	/*
+	int alleatoire = rand();
+	cout << alleatoire;
+	*/
+	/*
+	srand(time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << rand() % (11)<<endl;
+	}
+	*/
+	/*
+	Monstre* machin = new Monstre("m");
+	cout << machin->getPositionX()<<endl<<machin->getPositionY();
+	*/
+
 }
